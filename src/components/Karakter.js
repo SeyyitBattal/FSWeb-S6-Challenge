c03229e03d382f845a26082693ba22911f21718a
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-08-04 19:35:54 +0300

    I get data

--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,23 @@
 // Karakter bileşeniniz buraya gelecek
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Karakter = () => {
+  const [karakterListesi, setKarakterListesi] = useState();
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then(function (response) {
+        console.log("Gelen veri: ", response.data);
+        setKarakterListesi(response.data);
+      })
+      .catch(function (error) {
+        console.error("Veriyi çekmede hata var");
+        console.log(error);
+      });
+  }, []);
+};
+
+export default Karakter;
